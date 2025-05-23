3cb786e85c3affa444e56d84350deb07d9dd0f41
eugeniusms <eugeniusms@gmail.com>
2025-05-23 23:20:39 +0700

    feat: try leetcode and mmlu verification

--- exploration/tasks/task.py
+++ exploration/tasks/task.py
@@ -5,6 +5,8 @@ TASK_DESCRIPTION = {
     "bigbenchhard": "You will answer a reasoning question. Think step by step. The last line of your response should be of the following format: 'Answer: $VALUE' where VALUE is a numerical value.",
     "gpqa": "Answer the following multiple choice question. The last line of your response should be of the following format: 'Answer: $LETTER' (without quotes) where LETTER is one of ABCD. Think step by step before answering.",
     "gsm8k": "You will answer a mathemetical reasoning question. Think step by step. The last line of your response should be of the following format: 'Answer: $VALUE' where VALUE is a numerical value.",
+    "leetcode": "You will solve a hard coding problem from LeetCode. You will be given a prompt describing a problem. You need to write a function that passes all the tests.",
+    "mmlu": "Given a multiple choice question, the goal is to select the correct final answer from the choices."
 }
 
 def get_task(dataset):
@@ -18,6 +20,7 @@ def get_task(dataset):
             task += sample["input"]
         case "gpqa":
             task += f"""
+            {sample["Question"]}
             A) {sample["Correct Answer"]}
             B) {sample["Incorrect Answer 1"]}
             C) {sample["Incorrect Answer 2"]}
@@ -25,5 +28,15 @@ def get_task(dataset):
             """
         case "gsm8k":
             task += sample["question"]
+        case "leetcode":
+            task += sample["content"]
+        case "mmlu":
+            task += f"""
+            {sample["question"]}
+            A) {sample["choices"][0]}
+            B) {sample["choices"][1]}
+            C) {sample["choices"][2]}
+            D) {sample["choices"][3]}
+            """
 
     return task
