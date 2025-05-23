fe32f97262429dacfb7643dfbdc93ee368aebf38
eugeniusms <eugeniusms@gmail.com>
2025-05-24 01:48:02 +0700

    feat: remove verifier (old)

--- textgrad/textgrad/verification/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import get_verifier, BaseVerifier
-from .process_verification import ProcessVerifier
-from .outcome_verification import OutcomeVerifier
-
-__all__ = ["get_verifier", "BaseVerifier", "ProcessVerifier", "OutcomeVerifier"]
