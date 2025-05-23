75ac12535d8d3bb4839076f7844c43bcaa537f54
eugeniusms <eugeniusms@gmail.com>
2025-05-23 17:51:16 +0700

    init: hard reset

---
+++ exploration/run.py
@@ -0,0 +1,11 @@
+QUERY = """
+Answer the following multiple choice question. The last line of your response should be of the following format: 'Answer: $LETTER' (without quotes) where LETTER is one of ABCD. Think step by step before answering.
+
+Two quantum states with energies E1 and E2 have a lifetime of 10^-9 sec and 10^-8 sec, respectively. We want to clearly distinguish these two energy levels. Which one of the following options could be their energy difference so that they can be clearly resolved?
+
+A) 10^-4 eV
+B) 10^-11 eV
+C) 10^-8 eV
+D) 10^-9 eV
+""".strip()
+
