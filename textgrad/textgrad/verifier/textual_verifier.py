0e672966ea0f7c655842b2cae25702afea23e997
eugeniusms <eugeniusms@gmail.com>
2025-06-15 01:12:24 +0700

    feat: finish logging

--- textgrad/textgrad/verifier/textual_verifier.py
+++ textgrad/textgrad/verifier/textual_verifier.py
@@ -62,14 +62,20 @@ class TextualVerifier(Verifier):
         (prediction & loss value) + optimized prediction + verification prompt => verified optimized prediction
         ```
         """
+
+        if self.enable_logging:
+            print("INFO:textgrad:TextualVerifier:verify Start verification ...")
         
         calculation_value = calculation.value
 
         # 1. Breakdown to steps with CoT
         if self.use_cot_generation:
-            updated_calculation = self._generate_cot_steps(calculation_value) # Format: "<Step> ... </Step>"
+            updated_calculation = self._generate_cot_steps(calculation_value) 
         else:
             updated_calculation = calculation_value
+        
+        if self.enable_logging:
+            print("INFO:textgrad:TextualVerifier:verify Generate calculation list ...")
 
         # 2. Convert CoT to list
         if self.use_step_breakdown:
@@ -77,18 +83,26 @@ class TextualVerifier(Verifier):
         else:
             step_breakdown = [updated_calculation] # Just only 1 step in no step breakdown flag
         
+        if self.enable_logging:
+            print("INFO:textgrad:TextualVerifier:verify Calculation list is ready ...")
+
         initial_context = f"These are previous context to help you verify calculation:\n{instance.value}\n"
         voted_variant_list = []
-        for step in step_breakdown:
+        for i, step in enumerate(step_breakdown):
             # 3. Generate variants
+            if self.enable_logging:
+                print(f"INFO:textgrad:TextualVerifier:verify Verify step {i+1}/{len(step_breakdown)} ...")
+
             context = initial_context.join(f"Step {i+1}: ```{voted_variant}```" for i, voted_variant in enumerate(voted_variant_list))
             generated_variants = self._generate_variants(instance=instance.value, 
                                                          instruction=instruction.value, 
                                                          previous_context=context,
-                                                         calculation=step)
+                                                         calculation=step,
+                                                         i=i+1)
             # 4. Vote variants
             voted_variant = self._majority_vote_variants(calculation=step, 
-                                                         generated_variants=generated_variants)
+                                                         generated_variants=generated_variants,
+                                                         i=i+1)
             
             voted_variant_list.append(voted_variant)
 
@@ -98,12 +112,14 @@ class TextualVerifier(Verifier):
         return Variable(verified_calculaton, requires_grad=True, role_description="verified calculation")
 
     # For Calculation
-    def _generate_cot_steps(self, calculation: str) -> List[str]:
-        generate_cot_prompt = COT_PROMPT.format(
-            calculation=calculation
-        )
-        generated_cot = self.engine(generate_cot_prompt)
-        return generated_cot
+    def _get_ready_calculation(self, calculation: str) -> List[str]:
+        updated_calculation = calculation
+        if self.use_cot_generation: 
+            generate_cot_prompt = COT_PROMPT.format(
+                calculation=calculation
+            )
+            updated_calculation = self.engine(generate_cot_prompt)
+        return updated_calculation
 
     # For Calculation
     def _convert_cot_format_to_list(self, cot_formatted: str) -> List[str]:
@@ -122,10 +138,10 @@ class TextualVerifier(Verifier):
         return cleaned_steps
 
     # Generate Verified Variants of Calculation
-    def _generate_variants(self, instance: str, instruction: str, previous_context: str, calculation: str) -> List[str]:
+    def _generate_variants(self, instance: str, instruction: str, previous_context: str, calculation: str, i: str) -> List[str]:
         generated_variants = []
-        for i in range(len(self.verification_task_prompts)):
-            print(f"Generating variant {i} ...")
+        for j in range(len(self.verification_task_prompts)):
+            print(f"INFO:textgrad:TextualVerifier:generate_variants Generating step {i} variant {j+1}/{len(self.verification_task_prompts)} ...")
 
             if not self.use_step_breakdown: # If no use_step_breakdown -> ""
                 previous_context = ""
@@ -135,7 +151,7 @@ class TextualVerifier(Verifier):
                 instruction=instruction,
                 previous_context=previous_context,
                 calculation=calculation,
-                verification_task_prompt=self.verification_task_prompts[i]
+                verification_task_prompt=self.verification_task_prompts[j]
             )
                 
             new_variant = self.engine(variant_prompt)
@@ -143,11 +159,15 @@ class TextualVerifier(Verifier):
 
         return generated_variants
 
-    def _majority_vote_variants(self, calculation: str, generated_variants: List[str]) -> str:
-        generated_variants_formatted = "\n".join(f"Variant {i+1}: ```{variant}```" for i, variant in enumerate(generated_variants))
+    def _majority_vote_variants(self, calculation: str, generated_variants: List[str], i: str) -> str:
+        if self.enable_logging:
+            print(f"INFO:textgrad:TextualVerifier:majority_vote_variants Run majority voting for step {i}...")
+
+        generated_variants_formatted = "\n".join(f"Variant {j+1}: ```{variant}```" for j, variant in enumerate(generated_variants))
         voting_prompt = MAJORITY_VOTING_PROMPT.format(
             calculation=calculation,
             generated_variants=generated_variants_formatted
         )
         voted_variant = self.engine(voting_prompt)
+
         return voted_variant
